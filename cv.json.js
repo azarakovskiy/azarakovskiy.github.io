5e7c6f4ef20b4bafdfa7f6acb5256489ad19ff02
Alexey Zarakovskiy <4928787+azarakovskiy@users.noreply.github.com>
2018-01-15 23:50:16 +0100

    First version of CV, without projects

---
+++ cv.json.js
@@ -0,0 +1,57 @@
+var cv = {
+    Profile: {
+        Alexey Zarakovskiy
+        www.linkedin.com/in/alexeyzarakovskiy
+        alexey.zarakovskiy@gmail.com
+        English, Russian - fluent; Dutch, Spanish, Polish - advanced
+    }
+    
+    Personal statement: 
+        Experienced Web Java developer with strong UI development background in the past.
+        Focused on a server side development, doesn't mind to touch client side if needed.
+        Open to challenges and new technologies, nothing-is-impossible-if-legal mindset.
+    
+    Skills: {
+        Java: [ Spring, Groovy, Grails, Tomcat ],
+        Testing: [ jUnit, EasyMock, Mockito, JMeter, Postman ],
+        Build & CI/CD: [ Maven, Gradle, Git, Docker, SVN, Jenkins, TeamCity ],
+        DB: [ Oracle, MySQL, JDBC, Hibernate ],
+        Web: [ HTML, JS, jQuery, CSS, Twitter Bootstrap, AngularJS ],
+        Methodologies: [ Scrum, Kanban, SAFe, RUP ],
+        Other 1: [ PowerShell, PHP, C# ],
+        Other 2: [ Jira, Windows, Linux, UX, UML, Codenvy ]
+    }
+    
+    Employment: { sorted descending
+        SDL Tridion: {
+            October 2015 - now; Amsterdam; www.sdl.com
+            Java Developer, led Java part of the project (see projects for details)
+            The company develops an enterprise content management & delivery system for Web.
+        }
+        EPAM Systems: {
+            June 2012 - September 2014 - September 2015; Russia & relocated to Poland; www.epam.com
+            Java Web Developer, multiple projects & roles
+            The company is a large outsourcer with branches across the globe.
+        }
+        LIC: {
+            June 2011 - June 2012; Russia
+            First job as Java Developer, Java position with parallel leading on the frontend (JS, HTML, CSS)
+            Enterprise Content Management system based on ECM Alfresco.
+        }
+        Orange: {
+            June 2009 - June 2011; Russia
+            JS/PHP developer, started as PHP developer, the focus moved later and remained on JavaScript
+            Web studio with short and middle-term projects to create web portals and sites for multiple clients.
+        }
+        Energy: {
+            July 2006 - June 2009; Russia
+            C++ developer, first job ever, part-time (70%) job while studying
+            Specializes in radio interception, analog-to-digital conversion, surveillance and monitoring systems.
+        }
+    }
+    
+    Education: {
+        B.S. Computer Science 2005 - 2009; M.S. Computer Science 2009 - 2011, Russia
+        Saint Petersburg State University of Intomational Technilogies, Mechanics and Optics (http://en.ifmo.ru/en)
+    }
+}
